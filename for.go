4ead7047186f58f20857f3eab12ecee856617f0c
朴伟雄 <2355468258@qq.com>
2018-12-11 14:49:32 +0800

    测试for循环变量作用域

--- for.go
+++ for.go
@@ -3,17 +3,26 @@ package main
 import "fmt"
 
 func main() {
-    /********************************/
-    /** label 标签先定义后使用
-    /********************************/
-    JLoop:
-        for i:=0;i<5;i++ {
-            for j:=0;j<10;j++{
-                if i == 4 {
-                    break JLoop
-                }
-                fmt.Println("i is %d,j is %d", i, j)
-            }
-        }
-    //
+	/********************************/
+	/** label 标签先定义后使用
+	  /********************************/
+	// JLoop:
+	//     for i:=0;i<5;i++ {
+	//         for j:=0;j<10;j++{
+	//             if i == 4 {
+	//                 break JLoop
+	//             }
+	//             fmt.Println("i is %d,j is %d", i, j)
+	//         }
+	//     }
+	//
+	i := 0
+	for i < 5 {
+		v := i
+		defer func() {
+			//v := i
+			fmt.Println(v)
+		}()
+		i++
+	}
 }
