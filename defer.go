3961d6e342fa0fa9237cd191b478c4ad224e4a1e
朴伟雄 <2355468258@qq.com>
2018-12-11 15:07:01 +0800

    defer test

--- defer.go
+++ defer.go
@@ -3,16 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 5; i++ {
-		defer func() {
-			fmt.Println(i)
-		}()
-	}
+	// for i := 0; i < 5; i++ {
+	// 	defer func() {
+	// 		fmt.Println(i)
+	// 	}()
+	// }
+
+	// for i := 0; i < 5; i++ {
+	// 	defer func(i int) {
+	// 		fmt.Println(i)
+	// 	}(i)
+	// }
 
 	for i := 0; i < 5; i++ {
-		defer func(i int) {
-			fmt.Println(i)
-		}(i)
+		defer func(n int) {
+			fmt.Println(n)
+		}(i * 2)
 	}
 
 	fmt.Println(deferTest())
